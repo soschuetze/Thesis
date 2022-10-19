0ed45d39e43727cc3ec6f0c044c287be5449c6f3
Sinclaire <63867081+soschuetze@users.noreply.github.com>
2022-10-19 12:33:14 -0400

    Update clean_tweets.py

--- clean_tweets.py
+++ clean_tweets.py
@@ -4,7 +4,11 @@ import numpy as np
 import re
 import os
 import cleantext
- 
+import string
+import nltk
+import re
+
+
 demoji.download_codes()
 
 def remove_emojis(tweets_df):
@@ -22,12 +26,24 @@ def replace_urls(tweets_df):
 
 	return tweets_df
 
+def remove_stop_words(tweets_df, stopwords):
+	tweets_df['text'] = tweets_df['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stopwords)]))
+
+def remove_punctuation(tweets_df):
+	tweets_df['text'] = tweets_df['text'].str.replace(r'[^\w\s]','')
+
 def main():
+	stopwords = nltk.corpus.stopwords.words('english')
+	punctuation = string.punctuation
+
 	tweets = pd.read_csv("tweets.csv", index_col = [0])
 	tweets_no_emojis = remove_emojis(tweets)
 	tweets_no_mentions = replace_mentions(tweets_no_emojis)
 	tweets_no_urls = replace_urls(tweets_no_mentions)
-	print(tweets_no_urls['text'].head())
-	tweets_no_urls.to_csv("tweets_cleaned.csv", sep=',', encoding='utf-8')
+	tweets_no_stop_words = remove_stop_words(tweets_no_urls, stopwords)
+	tweets_no_punctuation = remove_punctuation(tweets_no_stop_words)
+	print(tweets_no_punctuation['text'].head())
+
+	tweets_no_punctuation.to_csv("tweets_cleaned.csv", sep=',', encoding='utf-8')
 
 main()
